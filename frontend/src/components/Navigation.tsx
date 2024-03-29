577ecffd993f20c11672a698f7e5c921d3284f6f
Nabuki <shichiromail@gmail.com>
2024-03-29 17:47:41 +0300

    design out mav bar

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -3,19 +3,24 @@ import { Link } from "react-router-dom";
 export function Navigation() {
   // TODO: log out, then goto /entry on Выйти
   return (
-    <nav className="h-[50px] flex justify-between px-5 bg-gray-500 items-center text-white">
-      <span className="font-bold">
-        <Link to="/feed" className="mr-2">
-          Афиша
+    <nav className="h-[100px] flex justify-between px-20 bg-white items-center">
+      <Link to="/feed">
+        <div className="text-blue-800 text-[40px] font-black font-Montserrat">
+          Aфиша
+        </div>
+      </Link>
+      <div className="flex items-center">
+        <Link to="/dashboard" className="mr-8">
+          <div className="text-black text-[32px] font-bold font-Montserrat">
+            Мои ивенты
+          </div>
         </Link>
-      </span>
-
-      <span>
-        <Link to="/dashboard" className="mr-2">
-          Мои ивенты
+        <Link to="/entry">
+          <div className="text-black text-[32px] font-semibold font-Montserrat">
+            выйти
+          </div>
         </Link>
-        <Link to="/entry">Выйти</Link>
-      </span>
+      </div>
     </nav>
   );
 }
