3c99eb824711743832c70384088db1083bdf3134
Nabuki <shichiromail@gmail.com>
2024-03-29 19:03:03 +0300

    reformulate todo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,10 @@ import EntryPage from "./pages/EntryPage";
 
 export default function App() {
   // TODO:
-  // if not logged in:
-  //   - force redirect to /entry
-  // if logged in:
-  //   - allow /feed /dashboard
-  //   - disallow /entry
-  //   - 404 or disallowed /entry - redirect to /feed
+  // /dashboard - if not logged in - redirect to /entry (ensureLoggedIn)
+  // /feed - if not logged in - redirect to /entry (ensureLoggedIn)
+  // /entry - if logged in - redirect to /feed (ensureNotLoggedIn)
+  // 404 - if not logged in - redirect to /entry, else redirect to /feed
   return (
     <Routes>
       <Route path="/entry" element={<EntryPage />} />
