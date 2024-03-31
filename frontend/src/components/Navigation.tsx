944f0f11b7b2b2c154a51c9cca10884074b27b6d
Nabuki <shichiromail@gmail.com>
2024-03-31 04:07:22 +0300

    fix hardcoded paths and proper log out

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,26 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { clearToken } from "../services/TokenStore";
+import * as Entry from "../pages/EntryPage";
+import * as Dashboard from "../pages/DashboardPage";
+import * as Feed from "../pages/FeedPage";
 
 export function Navigation({ loggedIn }: { loggedIn: boolean }) {
-  // TODO: log out, then goto /entry on Выйти
+  const navigate = useNavigate();
+
+  const exit = () => {
+    clearToken();
+    navigate(Entry.path);
+  };
+
   return (
     <nav className="h-[100px] flex justify-between px-20 bg-white items-center">
-      <Link to="/feed">
+      <Link to={Feed.path}>
         <div className="text-blue-800 text-[40px] font-black font-Montserrat">
           Aфиша
         </div>
       </Link>
       {loggedIn && (
         <div className="flex items-center">
-          <Link to="/dashboard" className="mr-8">
+          <Link to={Dashboard.path} className="mr-8">
             <div className="text-black text-[32px] font-bold font-Montserrat">
               Мои ивенты
             </div>
           </Link>
-          <Link to="/entry">
-            <div className="text-black text-[32px] font-semibold font-Montserrat">
-              выйти
-            </div>
-          </Link>
+          <button
+            className="text-black text-[32px] font-semibold font-Montserrat"
+            onClick={exit}
+          >
+            выйти
+          </button>
         </div>
       )}
     </nav>
--- frontend/src/services/TokenStore.ts
+++ frontend/src/services/TokenStore.ts
@@ -4,6 +4,10 @@ export function setToken(token: string) {
   localStorage.setItem(key, token);
 }
 
+export function clearToken() {
+  localStorage.removeItem(key);
+}
+
 export function getToken() {
   return localStorage.getItem(key);
 }
