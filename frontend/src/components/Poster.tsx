3aa77cc1a2c1f1c560d636c7b8cd4a6c12dcb0d5
Nabuki <shichiromail@gmail.com>
2024-04-22 21:44:58 +0300

    fix poster

--- frontend/src/components/Poster.tsx
+++ frontend/src/components/Poster.tsx
@@ -22,11 +22,10 @@ export function Poster({
     <div className="w-[365px] h-64 relative">
       <div className="w-[342px] h-[191px] left-0 top-0 absolute bg-blue-800 rounded-[20px]">
         <button
-          className="w-[143px] h-[34px] absolute right-0 bottom-0 mr-3 mb-7"
+          className="w-[130px] h-[34px] absolute right-0 bottom-0 mr-3 mb-3 bg-amber-500 rounded-[10px]"
           onClick={toggle}
         >
-          <div className="w-[143px] h-[34px] absolute bg-amber-500 rounded-[10px]" />
-          <div className="absolute left-[22px] top-[22px] text-white text-base font-bold font-Montserrat">
+          <div className="left-[22px] top-[22px] text-white text-base font-bold font-Montserrat">
             {isEnrolled ? "Вы записаны" : "Записаться"}
           </div>
         </button>
